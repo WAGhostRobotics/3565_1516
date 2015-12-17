e6e30b44cb1af849beafadc2935c85682ae6b515
16lheinold <lilibug156@gmail.com>
2015-12-17 15:01:36 -0500

    Update TeleOp1.java

--- Teleop/TeleOp1.java
+++ Teleop/TeleOp1.java
@@ -7,78 +7,106 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
 /**
- * Created by Lili on 12/8/2015.
+ * Created by 7876 on 12/9/2015.
+ *
+ * Authors: Lili,
+ *
+ * Teleop 3565
+ *
+ * Versions: (ADD AS YOU EDIT)
+ * (Big changes.small changes)
+ * 1.0: Added first versions of methods, untested. Completed 12/17/15. Contributors: Lili
  */
 public class TeleOp1 extends OpMode {
-    DcMotor motorLeft;
-    DcMotor motorRight;
-    DcMotor grabLeft;
-    DcMotor grabRight;
-    Servo buttonLeft;
-    Servo buttonRight;
-    Servo dump;
+    //Declare motors and servos
+    private DcMotor motorLeft;
+    private DcMotor motorRight;
+    private Servo buttonLeft;
+    private Servo buttonRight;
+    private Servo dump;
+    private Servo grab;
 
-    double buttonLeftPostion = 0;
-    double buttonRightPosition = 0;
-    double dumpPostion = 0;
-    double buttonDelta = 0.01;
-    double dumpDelta = 0.01;
+    //Declare variables
+    double buttonLeftPostion;
+    double buttonRightPosition;
+    double dumpPostion;
+    double buttonDelta;
+    double dumpDelta;
 
     public void loop(){
+        //Get joystick values
         double y2 = gamepad1.left_stick_y;
-        // double x2 = gamepad1.left_stick_x;
         double y = gamepad1.right_stick_y;
-        // double x = gamepad1.left_stick_x;
 
+        //Set the motor power
         motorLeft.setPower(y);
         motorRight.setPower(y2);
 
-        if(gamepad1.left_bumper){
+        if(gamepad1.left_bumper){ //Button left forward
             buttonLeftPostion += buttonDelta;
         }
 
-        if(gamepad1.right_bumper){
+        if(gamepad1.right_bumper){ //Button left backward
             buttonLeftPostion -= buttonDelta;
         }
 
-        if(gamepad1.a){
+        if(gamepad1.a){ //Button right forward
             buttonRightPosition += buttonDelta;
         }
 
-        if(gamepad1.b){
+        if(gamepad1.b){ //Button right backward
             buttonRightPosition -= buttonDelta;
         }
 
-        if(gamepad2.right_bumper) {
+        //DOESN'T WORK
+        if(gamepad2.right_bumper) { //Dump forward
             dumpPostion += dumpDelta;
+            dumpPostion = Range.clip(dumpPostion, 0, 1);
+            telemetry.addData("dump position",  ": " + String.format("%.2f", dumpPostion));
+            dump.setPosition(dumpPostion);
+            telemetry.addData("dump position actual", ": " + String.format("%.2f", dump.getPosition()));
         }
 
-        if(gamepad2.left_bumper) {
-            dumpPostion -= dumpDelta;
+        if(gamepad2.left_bumper) { //Dump backward
+            dumpPostion = Range.clip(dumpPostion, 0, 1);
+            telemetry.addData("dump position",  ": " + String.format("%.2f", dumpPostion));
+            dump.setPosition(dumpPostion);
+            telemetry.addData("dump position actual", ": " + String.format("%.2f", dump.getPosition()));
         }
 
-        buttonLeftPostion = Range.clip(buttonLeftPostion, 0, 1);
-        buttonRightPosition = Range.clip(buttonRightPosition, 0, 1);
-        dumpPostion = Range.clip(dumpPostion, 0, 1);
+        //Clip range
+       // buttonLeftPostion = Range.clip(buttonLeftPostion, 0, 1);
+       // buttonRightPosition = Range.clip(buttonRightPosition, 0, 1);
+       // dumpPostion = Range.clip(dumpPostion, 0, 1);
 
-        telemetry.addData("dump position",  ": " + String.format("%.2f", dumpPostion));
+        //(error checking :( )
+        //telemetry.addData("dump position",  ": " + String.format("%.2f", dumpPostion));
 
-        //  buttonLeft.setPosition(buttonLeftPostion);
-        //buttonRight.setPosition(buttonRightPosition);
-        dump.setPosition(dumpPostion);
+        //Set servo positions
+//        buttonLeft.setPosition(buttonLeftPostion);
+  //      buttonRight.setPosition(buttonRightPosition);
+    //    dump.setPosition(dumpPostion);
 
     }
 
     public void init(){
+        //Initialize motors
         motorRight = hardwareMap.dcMotor.get("motor_2");
         motorLeft = hardwareMap.dcMotor.get("motor_1");
-        //grabLeft = hardwareMap.dcMotor.get("grabLeft");
-        //grabRight = hardwareMap.dcMotor.get("grabRight");
         motorRight.setDirection(DcMotor.Direction.REVERSE);
 
-        //buttonLeft = hardwareMap.servo.get("servo_3"); // channel 3
-        //buttonRight = hardwareMap.servo.get("servo_2"); // channel 2
+        //Initialize servos
+        buttonLeft = hardwareMap.servo.get("buttonl"); // channel 3
+        buttonRight = hardwareMap.servo.get("buttonr"); // channel 2
         dump = hardwareMap.servo.get("dump"); // channel 1
+        //grab = hardwareMap.servo.get("servo_4"); //channel 4
+
+        //Initialize variables
+        buttonLeftPostion = 0;
+        buttonRightPosition = 0;
+        dumpPostion = 0;
+        buttonDelta = 0.01;
+        dumpDelta = 0.01;
     }
 
 }
